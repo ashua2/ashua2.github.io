97d09c7eebcd15466b117f906f6c8ad4d89b4b00
Adriana Shuaipaj <ashua2@illinois.edu>
2023-07-24 17:39:03 -0500

    reformatting data

--- charts/chart.js
+++ charts/chart.js
@@ -22,11 +22,8 @@ d3.select("#dropdown")
     .style("font-family", "tahoma");
 
 // dataset cleaning + storing
-
 var parseTime = d3.timeParse('%m-%d%-%Y');
 var formatTime = d3.timeFormat("%B %d, %Y");
-var races = ["Black", "Asian", "Latinx", "White", "Other"];
-var colors = ["orange", "violet", "cornflowerblue", "crimson", "mediumaquamarine"];
 
 d3.csv("https://gist.githubusercontent.com/ashua2/c369a7bbca9311c50632a9a9c138f30d/raw/a381a14cc3838487451cc90cb5b9476c6e4cca3d/covid_deaths_cases_by_race.csv").then(function(dataset) {
     console.log(dataset);
@@ -35,192 +32,185 @@ d3.csv("https://gist.githubusercontent.com/ashua2/c369a7bbca9311c50632a9a9c138f3
         svg.selectAll("*").remove();
         if (selectedGroup == factors[0]) { // line chart by ethnicity
             var dates_deaths_race = [];
+            var races = ["Black", "Asian", "Latinx", "White", "Other"];
+            var colors = ["orange", "violet", "cornflowerblue", "crimson", "mediumaquamarine"];
+            var attributes = ["latinx_deaths", "asian_deaths", "black_deaths", "white_deaths", "other_deaths"];
 
-        dataset.forEach(function(data) {
-            let race_deaths = {
-                "date": new Date(+(data.date.slice(6)), +(data.date.slice(0,2) - 1), +(data.date.slice(3,5))),
-                "latinx_deaths": +data.deaths_latinx,
-                "asian_deaths": +data.deaths_asian_nonlat,
-                "black_deaths": +data.deaths_black_nonlat,
-                "white_deaths": +data.deaths_white_nonlat,
-                "other_deaths": +data.deaths_other_nonlat
-            }
-            if (race_deaths.date <= new Date(2021, 2, 1) && race_deaths.date >= new Date(2020, 2, 1)) {
+            dataset.forEach(function(data) {
+                let race_deaths = {
+                    "date": new Date(+(data.date.slice(6)), +(data.date.slice(0,2) - 1), +(data.date.slice(3,5))),
+                    "latinx_deaths": +data.deaths_latinx,
+                    "asian_deaths": +data.deaths_asian_nonlat,
+                    "black_deaths": +data.deaths_black_nonlat,
+                    "white_deaths": +data.deaths_white_nonlat,
+                    "other_deaths": +data.deaths_other_nonlat
+                }
+                if (race_deaths.date <= new Date(2021, 2, 1) && race_deaths.date >= new Date(2020, 2, 1)) {
                 dates_deaths_race.push(race_deaths);
+                }
+            });
+            dates_deaths_race.sort(function(o1,o2){
+                return o1.date - o2.date;
+            });
+            final_deaths_race = dates_deaths_race;
+            for (let i = 0; i < 5; i++) {
+                final_deaths_race.push(dates_deaths_race);
             }
-        });
-        dates_deaths_race.sort(function(o1,o2){
-            return o1.date - o2.date;
-        });
 
-    // axes + labels
-        var dates_x_axis = d3.scaleTime()
-            .domain([new Date(2020, 2, 1), new Date(2021, 2, 1)])
-            .range([0, width]);
+            console.log(final_deaths_race);
 
-        var deaths_y_axis = d3.scaleLinear()
-            .domain([0, 30])
-            .range([height, 0]);
+        // axes + labels
+            var dates_x_axis = d3.scaleTime()
+                .domain([new Date(2020, 2, 1), new Date(2021, 2, 1)])
+                .range([0, width]);
 
-        svg.append("g")
-            .call(d3.axisLeft(deaths_y_axis));
+            var deaths_y_axis = d3.scaleLinear()
+                .domain([0, 30])
+                .range([height, 0]);
 
-        svg.append("g")
-            .attr("transform","translate(0,"+height+")")
-            .call(d3.axisBottom(dates_x_axis));
+            svg.append("g")
+                .call(d3.axisLeft(deaths_y_axis));
 
-        svg.append('text')
-            .attr('x', width/2).attr('y', height + 40).attr('text-anchor', 'middle')
-            .style('font-family', 'tahoma').style('font-size', 15)
-            .text('Month');
+            svg.append("g")
+                .attr("transform","translate(0,"+height+")")
+                .call(d3.axisBottom(dates_x_axis));
 
-        svg.append('text')
-            .attr('text-anchor', 'middle').attr('transform', 'translate(-35,' + height/2 + ')rotate(-90)')
-            .style('font-family', 'tahoma').style('font-size', 15)
-            .text('Deaths');
+            svg.append('text')
+                .attr('x', width/2).attr('y', height + 40).attr('text-anchor', 'middle')
+                .style('font-family', 'tahoma').style('font-size', 15)
+                .text('Month');
 
-    // colored lines
-        svg.append("path")
-            .datum(dates_deaths_race)
-            .attr("fill", "none")
-            .attr("stroke", "cornflowerblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return dates_x_axis(d.date); })
-                .y(function(d) { return deaths_y_axis(d.latinx_deaths); }));
+            svg.append('text')
+                .attr('text-anchor', 'middle').attr('transform', 'translate(-35,' + height/2 + ')rotate(-90)')
+                .style('font-family', 'tahoma').style('font-size', 15)
+                .text('Deaths');
 
-        svg.append("path")
-            .datum(dates_deaths_race)
-            .attr("fill", "none")
-            .attr("stroke", "violet")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return dates_x_axis(d.date); })
-                .y(function(d) { return deaths_y_axis(d.asian_deaths); }));
+        // colored lines
+            svg.append("path")
+                .datum(dates_deaths_race)
+                .attr("fill", "none")
+                .attr("stroke", "cornflowerblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(function(d) { return dates_x_axis(d.date); })
+                    .y(function(d) { return deaths_y_axis(d.latinx_deaths); }));
 
-        svg.append("path")
-            .datum(dates_deaths_race)
-            .attr("fill", "none")
-            .attr("stroke", "orange")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return dates_x_axis(d.date); })
-                .y(function(d) { return deaths_y_axis(d.black_deaths); }));
+            svg.append("path")
+                .datum(dates_deaths_race)
+                .attr("fill", "none")
+                .attr("stroke", "violet")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(function(d) { return dates_x_axis(d.date); })
+                    .y(function(d) { return deaths_y_axis(d.asian_deaths); }));
 
-        svg.append("path")
-            .datum(dates_deaths_race)
-            .attr("fill", "none")
-            .attr("stroke", "crimson")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return dates_x_axis(d.date); })
-                .y(function(d) { return deaths_y_axis(d.white_deaths); }));
+            svg.append("path")
+                .datum(dates_deaths_race)
+                .attr("fill", "none")
+                .attr("stroke", "orange")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(function(d) { return dates_x_axis(d.date); })
+                    .y(function(d) { return deaths_y_axis(d.black_deaths); }));
 
-        svg.append("path")
-            .datum(dates_deaths_race)
-            .attr("fill", "none")
-            .attr("stroke", "mediumaquamarine")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return dates_x_axis(d.date); })
-                .y(function(d) { return deaths_y_axis(d.other_deaths); }));
+            svg.append("path")
+                .datum(dates_deaths_race)
+                .attr("fill", "none")
+                .attr("stroke", "crimson")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(function(d) { return dates_x_axis(d.date); })
+                    .y(function(d) { return deaths_y_axis(d.white_deaths); }));
 
-    // legend
-        svg.selectAll("legenddots")
-            .data(colors).enter().append("circle")
-                .attr("cx", 795)
-                .attr("cy", function(d,i) {return 20 + i*20;})
-                .attr("r", 3)
-                .style("fill", function(d,i) {return d;});
+            svg.append("path")
+                .datum(dates_deaths_race)
+                .attr("fill", "none")
+                .attr("stroke", "mediumaquamarine")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(function(d) { return dates_x_axis(d.date); })
+                    .y(function(d) { return deaths_y_axis(d.other_deaths); }));
 
-        svg.selectAll("legendlabels")
-            .data(races).enter().append("text")
-                .attr("x", 810)
-                .attr("y", function(d,i) {return 20 + i*20;})
-                .attr("alignment-baseline","middle")
-                .text(function(d) {return d;})
-                .style("font-size", "13px").style("font-family", "tahoma");
+        // legend
+            svg.selectAll("legenddots")
+                .data(colors).enter().append("circle")
+                    .attr("cx", 795)
+                    .attr("cy", function(d,i) {return 20 + i*20;})
+                    .attr("r", 3)
+                    .style("fill", function(d,i) {return d;});
 
-    // annotations
-        //black deaths annotation
-        svg.append("text")
-            .attr("x", 140)
-            .attr("y", 5)
-            .html("Black deaths were at an all-time high on "+formatTime(dates_deaths_race[52].date)+", with a total of "+dates_deaths_race[52].black_deaths+" deaths.")
-            .style("font-size", "10px").style("font-family", "verdana").style("stroke", "darkorange").style("letter-spacing", 1.5);
-        svg.append('line')
-            .style("stroke", "darkorange")
-            .style("stroke-width", 1)
-            .attr("x1", 120)
-            .attr("y1", 35)
-            .attr("x2", 140)
-            .attr("y2", 10);
+            svg.selectAll("legendlabels")
+                .data(races).enter().append("text")
+                    .attr("x", 810)
+                    .attr("y", function(d,i) {return 20 + i*20;})
+                    .attr("alignment-baseline","middle")
+                    .text(function(d) {return d;})
+                    .style("font-size", "13px").style("font-family", "tahoma");
 
-        //latinx deaths annotation
-        svg.append("text")
-            .attr("x", 190)
-            .attr("y", 40)
-            .html("Latinx deaths were at an all-time high on "+formatTime(dates_deaths_race[65].date)+", with a total of "+dates_deaths_race[65].latinx_deaths+" deaths.")
-            .style("font-size", "10px").style("font-family", "verdana").style("stroke", "royalblue").style("letter-spacing", 1.5).style("font-weight", 100);
-        svg.append("text")
-            .attr("x", 185)
-            .attr("y", 53)
-            .html("This point begins the trend of Latinx deaths generally being the highest of all ethnicities.")
-            .style("font-size", "10px").style("font-family", "verdana").style("stroke", "royalblue").style("letter-spacing", 1.5).style("font-weight", 100);
+        // annotations
+            //black deaths annotation
+            svg.append("text")
+                .attr("x", 140)
+                .attr("y", 5)
+                .html("Black deaths were at an all-time high on "+formatTime(dates_deaths_race[52].date)+", with a total of "+dates_deaths_race[52].black_deaths+" deaths.")
+                .style("font-size", "10px").style("font-family", "verdana").style("stroke", "darkorange").style("letter-spacing", 1.5);
+            svg.append('line')
+                .style("stroke", "darkorange")
+                .style("stroke-width", 1)
+                .attr("x1", 120)
+                .attr("y1", 35)
+                .attr("x2", 140)
+                .attr("y2", 10);
 
-        svg.append('line')
-            .style("stroke", "royalblue")
-            .style("stroke-width", 1)
-            .attr("x1", 185)
-            .attr("y1", 40)
-            .attr("x2", 160)
-            .attr("y2", 50);
+            //latinx deaths annotation
+            svg.append("text")
+                .attr("x", 190)
+                .attr("y", 40)
+                .html("Latinx deaths were at an all-time high on "+formatTime(dates_deaths_race[65].date)+", with a total of "+dates_deaths_race[65].latinx_deaths+" deaths.")
+                .style("font-size", "10px").style("font-family", "verdana").style("stroke", "royalblue").style("letter-spacing", 1.5).style("font-weight", 100);
+            svg.append("text")
+                .attr("x", 185)
+                .attr("y", 53)
+                .html("This point begins the trend of Latinx deaths generally being the highest of all ethnicities.")
+                .style("font-size", "10px").style("font-family", "verdana").style("stroke", "royalblue").style("letter-spacing", 1.5).style("font-weight", 100);
 
-        //white deaths annotation
-        svg.append("text")
-            .attr("x", 225)
-            .attr("y", 170)
-            .html("Some of the few times when white deaths exceeded all other ethnicities.")
-            .style("font-size", "10px").style("font-family", "verdana").style("stroke", "maroon").style("letter-spacing", 1.5).style("font-weight", 100);
+            svg.append('line')
+                .style("stroke", "royalblue")
+                .style("stroke-width", 1)
+                .attr("x1", 185)
+                .attr("y1", 40)
+                .attr("x2", 160)
+                .attr("y2", 50);
 
-        svg.append('line')
-            .style("stroke", "maroon")
-            .style("stroke-width", 1)
-            .attr("x1", 570)
-            .attr("y1", 293)
-            .attr("x2", 450)
-            .attr("y2", 185);
-        svg.append('line')
-            .style("stroke", "maroon")
-            .style("stroke-width", 1)
-            .attr("x1", 505)
-            .attr("y1", 355)
-            .attr("x2", 450)
-            .attr("y2", 185);
+            //white deaths annotation
+            svg.append("text")
+                .attr("x", 225)
+                .attr("y", 170)
+                .html("Some of the few times when white deaths exceeded all other ethnicities.")
+                .style("font-size", "10px").style("font-family", "verdana").style("stroke", "maroon").style("letter-spacing", 1.5).style("font-weight", 100);
+
+            svg.append('line')
+                .style("stroke", "maroon")
+                .style("stroke-width", 1)
+                .attr("x1", 570)
+                .attr("y1", 293)
+                .attr("x2", 450)
+                .attr("y2", 185);
+            svg.append('line')
+                .style("stroke", "maroon")
+                .style("stroke-width", 1)
+                .attr("x1", 505)
+                .attr("y1", 355)
+                .attr("x2", 450)
+                .attr("y2", 185);
         } else if (selectedGroup == factors[1]) {
-            d3.select("svg").append("circle")
-                .attr("cx", 200)
-                .attr("cy", 150)
-                .attr("r", 10)
-                .attr("fill", "peru");
+            // chart #2
         } else if (selectedGroup == factors[2]) {
-            svg.append("circle")
-                .attr("cx", 300)
-                .attr("cy", 200)
-                .attr("r", 10)
-                .attr("fill", "plum");
+            // chart #3
         } else if (selectedGroup == factors[3]) {
-            svg.append("circle")
-                .attr("cx", 400)
-                .attr("cy", 250)
-                .attr("r", 10)
-                .attr("fill", "powderblue");
+            // chart #4
         } else {
-            svg.append("circle")
-                .attr("cx", 450)
-                .attr("cy", 270)
-                .attr("r", 10)
-                .attr("fill", "rosybrown");
+            // remember to use svg.append(...) NOT d3.select("svg") :)
         }
     }
 
